325e4f76226d4aa94df55217969ef1f2a4931ff0
Edwin Alexander Vargas <edwinalexandervargas07@gmail.com>
2026-05-02 22:51:30 -0700

    fix contact form and email validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
       .then(
         () => {
           setStatus("success");
-          e.target.rest()
+          e.target.reset()
         },
         (error) => {
           setStatus("error");
