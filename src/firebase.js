365ce8651a82d6b30b246fc437af2dcf4e922a31
Edwin Alexander Vargas <edwinalexandervargas07@gmail.com>
2026-05-11 09:37:33 -0700

    hide firebase config in environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,15 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXmwfVujOYxhqMTrXG7QZQeXOba0g6P1U",
-  authDomain: "ap-plumbing-91865.firebaseapp.com",
-  projectId: "ap-plumbing-91865",
-  storageBucket: "ap-plumbing-91865.firebasestorage.app",
-  messagingSenderId: "226790221838",
-  appId: "1:226790221838:web:1291a4348d904af590b45f"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+
